43bdfa457059032e8cc599a8e9bae2e8a4a7f088
Vinod Rahal <smvinodrahal@gmail.com>
2025-03-20 16:05:34 +0530

    add routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/homePage.dart';
 
 void main() {
-  runApp(Scaffold(
-    appBar: AppBar(
-      title: Text('Hello World'),
-    ),
-    body: Center(
-      child: Text('Hello World'),
-    ),
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => Home(),
+    },
   ));
 }
 
