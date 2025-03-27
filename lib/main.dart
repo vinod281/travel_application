02559c0885ae129ec00d8fc97fa1722e678e64d0
Ronath1 <ronathwijayasiri123@gmail.com>
2025-03-27 16:08:20 +0530

    develop view page

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/ViewPage.dart';
 import 'package:travel_app/homePage.dart';
 
 void main() {
   runApp(MaterialApp(
-    initialRoute: '/',
+    initialRoute: '/viewPage',
     routes: {
       '/': (context) => Home(),
+      '/viewPage':(context) => ViewPage()
     },
   ));
 }
